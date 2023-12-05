71be7c22af463526d9b937d1a242bca07f92094b
elyar <elyarnejati@gmail.com>
2023-12-06 00:28:18 +0330

    Added Cors Origin

--- cmd/search-api/routes.go
+++ cmd/search-api/routes.go
@@ -3,11 +3,24 @@ package main
 import (
 	"net/http"
 
+	"github.com/rs/cors"
 	"github.com/gorilla/mux"
 )
 
 func (app *Config) routes() http.Handler {
 	router := mux.NewRouter()
+
+	corsHandler := cors.New(cors.Options{
+		AllowedOrigins:   []string{"http://localhost:3000", "https://localhost:3000", "http://*"},
+		AllowedMethods:   []string{"GET", "OPTIONS", "HEAD"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	})
+
+	// Apply CORS middleware to the router
+	router.Use(corsHandler.Handler)
 
 	router.HandleFunc("/api/search/books", app.SearchBooksByNameHandler).Methods("GET").Queries("bookName", "{bookName}")
 	router.HandleFunc("/api/search/books", app.SearchBooksByAuthorHandler).Methods("GET").Queries("author", "{author}")
